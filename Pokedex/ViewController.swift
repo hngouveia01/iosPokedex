65e332b9f5a84b7dd7ca9e4940c524f39c2c8972
Henrique Gouveia <h.gouveia@icloud.com>
2020-04-15 18:24:37 -0300

    searchbar: dismiss keyboard after tapping outside
    Format search text and style.

--- Pokedex/ViewController.swift
+++ Pokedex/ViewController.swift
@@ -17,17 +17,30 @@ class ViewController:  UIViewController, UISearchBarDelegate {
         /* Setup delegates */
         self.search.delegate = self
         self.pokemonImage.setValue(UIColor.systemGreen, forKey: "backgroundColor")
+        let singleTapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.singleTap(sender:)))
+        singleTapGestureRecognizer.numberOfTapsRequired = 1
+        singleTapGestureRecognizer.isEnabled = true
+        singleTapGestureRecognizer.cancelsTouchesInView = false
+        self.view.addGestureRecognizer(singleTapGestureRecognizer)
+    }
+
+    @objc func singleTap(sender: UITapGestureRecognizer) {
+        self.search.resignFirstResponder()
     }
 
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         if searchBar.text!.isEmpty {
             self.pokemonImage.image = nil
+            searchBar.endEditing(true)
+            searchActive = false
             return
         }
         if let pokemonName = searchBar.text
         {
             self.pokemonImage.image = nil
+
             let pokeNameLower: String = pokemonName.lowercased()
+
             api.fetchPokemon(name: pokeNameLower).then { pokemon in
                 self.api.fetchPokemonImage(imageURL: URL(string: pokemon.sprites.frontDefault)!)
             }.done { image in
@@ -37,7 +50,9 @@ class ViewController:  UIViewController, UISearchBarDelegate {
                 self.pokemonImage.image = nil
             }
         }
+        searchActive = false
         searchBar.endEditing(true)
+        searchBar.resignFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
